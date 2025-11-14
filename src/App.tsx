fdb423fe465a42700551e2143e8d3c4a9cb518d0
phantekzy <mainilotfi@gmail.com>
2025-11-14 18:59:34 +0100

    created brands section , created a shared title component , added a logos folder for the brands section

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 /* Import section */
 import { Layout } from "./components/Layout"
+import { Brands } from "./components/section/Brands"
 import { Hero } from "./components/section/Hero"
 
 /* App Component section */
@@ -8,6 +9,8 @@ function App() {
     return <Layout title="ApexVerse">
         {/* Hero section */}
         <Hero />
+        {/* Brands section */}
+        <Brands />
     </Layout>
 }
 /* Export section */
---
+++ src/components/section/Brands.tsx
@@ -0,0 +1,33 @@
+/* Import section */
+import { Container } from "../shared/Container"
+import { Title } from "../shared/Title"
+/* Logos section */
+const logos = [
+    "discord",
+    "google",
+    "mercedes",
+    "microsoft",
+    "openai",
+    "spotify",
+    "gemini",
+]
+/* Brands component */
+export const Brands = () => {
+    return <section>
+        <Container className="space-y-8 ">
+            <div className="text-center max-w-3xl mx-auto">
+                <Title>Trusted by leading companies worldwide</Title>
+            </div>
+            <div className="flex gap-8 justify-center items-center flex-wrap">
+                {logos.map((logo, key) => (
+                    <div key={key} className="p-4 sm:p-5 rounded-xl bg-body  group">
+                        <img src={`src/assets/logos/${logo}.png`} width="100" height="60"
+                            className="h-7 sm:h-10 w-auto ease-linear duration-300 grayscale group-hover:!grayscale-0 
+                            group-hover:scale-105"
+                        />
+                    </div>
+                ))}
+            </div>
+        </Container>
+    </section>
+}
