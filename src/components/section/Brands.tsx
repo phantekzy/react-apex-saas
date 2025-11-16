48ad3d82e3a47b3d42baadeb561175adc2ea8c6c
phantekzy <mainilotfi@gmail.com>
2025-11-16 16:59:24 +0100

    i fixed the bug in the brands section , the logos was not showing up i had to put them in the public folder

--- src/components/section/Brands.tsx
+++ src/components/section/Brands.tsx
@@ -20,7 +20,7 @@ export const Brands = () => {
             <div className="flex gap-8 justify-center items-center flex-wrap">
                 {logos.map((logo, key) => (
                     <div key={key} className="p-4 sm:p-5 rounded-xl bg-body  group">
-                        <img src={`src/assets/logos/${logo}.png`} width="100" height="60"
+                        <img src={`/logos/${logo}.png`} width="100" height="60"
                             className="h-8 sm:h-15 w-auto ease-linear duration-300 grayscale group-hover:grayscale-0! 
                             group-hover:scale-105"
                         />
